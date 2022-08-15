2fd95075a0d50190dbd01f3adac81e0b99c392f9
Jeremy <jeremy__francis@hotmail.com>
2022-08-15 11:59:00 -0700

    commit tree feabd3316aba8c2d2c06a2072eea0912ab78b4e5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true, inclusion: {in: ["Won't Believe" || "Secret" || "Guess"]}
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}
+
+
+    
 end
